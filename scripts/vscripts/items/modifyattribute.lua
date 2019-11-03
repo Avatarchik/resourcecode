92cbde19f6306e33dc4f3c2f0d049fbd3b873e5a
MakingPanorama <50879571+MakingPanorama@users.noreply.github.com>
2019-11-03 20:28:33 +0200

    Changed Structure Books

--- scripts/vscripts/items/modifyattribute.lua
+++ scripts/vscripts/items/modifyattribute.lua
@@ -1,3 +1,9 @@
+--[[
+Author: DDSuper
+Item: Book of Strength / Book of Agillity / Book of Intellect
+Data: 10.28.2019
+--]]
+
 function ChangeAtribute(keys)
 	keys.caster:ModifyAgility(keys.BonusAgillity)
 end
